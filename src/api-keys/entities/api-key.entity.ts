2c7e505dab17b1ce62839c946eda4c62ba4e8b23
Joy <joykenneth605@gmail.com>
2026-06-20 00:28:01 +0100

    chore: create api key entity to define the structure of the api key table

--- src/api-keys/entities/api-key.entity.ts
+++ src/api-keys/entities/api-key.entity.ts
@@ -1,1 +1,16 @@
-export class ApiKey {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class ApiKey {
+    @PrimaryGeneratedColumn('uuid')
+    id!: string;
+
+    @Column()
+    name!: string;
+
+    @Column({ default: 1000 })
+    rateLimitPerMinute!: number;
+
+    @CreateDateColumn()
+    createdAt!: Date;
+}
