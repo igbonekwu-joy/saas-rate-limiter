daf04f0db3e84ab1dcb5446dbe2a36e6ee5774a2
Joy <joykenneth605@gmail.com>
2026-06-19 23:54:28 +0100

    chore: install and register typeorm and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiKeysModule } from './api-keys/api-keys.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ApiKeysModule],
+  imports: [
+    ApiKeysModule,
+    ConfigModule.forRoot({ isGlobal: true }), // loads .env, available everywhere
+
+    TypeOrmModule.forRootAsync({ // async because it needs to wait for configservice to load .env
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // dev only . typeorm automatically creates/updates tables to match entities
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
