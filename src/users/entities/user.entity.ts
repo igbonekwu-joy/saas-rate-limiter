5b7230cf4606476bd77e0c9f448e080761f3428b
Joy <joykenneth605@gmail.com>
2026-06-23 21:41:48 +0100

    feat(auth): remove authentication completely from app

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -12,6 +12,9 @@ export class User {
     @Column()
     password!: string;
 
+    @Column({ nullable: true })
+    refreshToken?: string;
+
     @OneToMany(() => ApiKey, (apiKey) => apiKey.user)
     apiKeys!: ApiKey[];
 
