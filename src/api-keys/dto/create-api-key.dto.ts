2484be5f795dc6b6d1315fd76a27f82ec16b5d13
Joy <joykenneth605@gmail.com>
2026-06-26 12:34:06 +0100

    feat: update api key entity to include burst limit and plans

--- src/api-keys/dto/create-api-key.dto.ts
+++ src/api-keys/dto/create-api-key.dto.ts
@@ -1,7 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
 
 export class CreateApiKeyDto {
     @ApiProperty({ example: 'My production key', description: 'A label to identify this key' })
+    @IsString()
     name!: string;
 
     @ApiProperty({ example: 1000, description: 'Requests allowed per minute' })
--- src/api-keys/entities/api-key.entity.ts
+++ src/api-keys/entities/api-key.entity.ts
@@ -1,6 +1,11 @@
 import { User } from "src/users/entities/user.entity";
 import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
+export enum Plan {
+    FREE = 'free',
+    PAID = 'paid'
+};
+
 @Entity()
 export class ApiKey {
     @PrimaryGeneratedColumn('uuid')
@@ -12,8 +17,16 @@ export class ApiKey {
     @Column()
     name!: string;
 
-    @Column({ default: 1000 })
+    @Column({ type: 'enum', enum: Plan, default: Plan.FREE })
+    plan!: Plan;
+
+    // per minute limit
+    @Column({ default: 100 })
     rateLimitPerMinute!: number;
+
+    // burst limit (per-second limit)
+    @Column({ default: 20 })
+    burstLimitPerSecond!: number;
 
     @ManyToOne(() => User, (user) => user.apiKeys)
     user!: User;
